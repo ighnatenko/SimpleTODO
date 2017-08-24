c630d78fbdef215d0c1a2d9fed7977b32b2d4026
Alexandr <ighnatenko_alexandr@meta.ua>
2017-08-25 00:39:36 +0300

    FIX projects/index.html

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :projects, only: [:index, :create, :update, :destroy] do
     resources :tasks, only: [:create, :update, :destroy] do
       collection { post :sort }
-      # post 'tasks/sort', to: 'tasks#sort', as: 'sort_tasks'
     end
   end
 
